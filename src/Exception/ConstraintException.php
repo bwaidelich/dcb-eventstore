354ace0d50623e0264e3850e6631a671b5cbed4a
bwaidelich <b.waidelich@wwwision.de>
2023-06-05 12:27:07 +0200

    Add ConstraintException

---
+++ src/Exception/ConstraintException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Wwwision\DCBEventStore\Exception;
+
+use InvalidArgumentException;
+
+final class ConstraintException extends InvalidArgumentException
+{
+
+}
